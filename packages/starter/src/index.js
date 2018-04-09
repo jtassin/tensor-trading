0d51df0c947415aa99b762db40270b343cd64da0
jtassin <jtassin@artprice.com>
2018-04-09 09:34:07 +0200

    replace it with a constant

--- packages/starter/src/index.js
+++ packages/starter/src/index.js
@@ -14,7 +14,7 @@ const runner = async () => {
   data = data.slice(0, data.length - 21);
 
   // Add a dense layer with 1 output unit.
-  model.add(tf.layers.dense({ units: 3, inputShape: [data[0].length - DAYS_SHIFT.length] }));
+  model.add(tf.layers.dense({ units: DAYS_SHIFT.length, inputShape: [data[0].length - DAYS_SHIFT.length] }));
 
   // Specify the loss type and optimizer for training.
   // optimizer was sgd
